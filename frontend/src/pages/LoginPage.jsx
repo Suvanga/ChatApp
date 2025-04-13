393e3b6d179425840cb282663c8c7e85938d61cc
Suvanga <suvanga.dhz@gmail.com>
2025-04-12 21:43:57 -0400

    The authentication part of the app with axios being connected cors installed at backend was done. Furthermore, the tailwind with daisyUI is being used for faster easier components being made, Lucide for loading animation, while base of the project is setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+  return (
+    <div className="text-white" >LoginPage</div>
+  )
+}
+
+export default LoginPage;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: c:\Users\dhaka\OneDrive\Desktop\ChatApp\tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
